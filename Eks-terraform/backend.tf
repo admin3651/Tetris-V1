b5f0b13a54d27b63f8812205aacd999c1f6635ac
admin3651 <net_admin365@hotmail.com>
2024-03-04 22:43:27 +0200

    sdf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mangroveslink-terraform" # Replace with your actual S3 bucket name
+    bucket = "mangroveslink-terraform-eks" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "eu-central-1"
   }
